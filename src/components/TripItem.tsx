7dabb8711cc04c3e7c85f95c06931d47ff52d1ac
Paulo Santana <paulosantana.sa@gmail.com>
2023-07-10 23:45:25 -0300

    feat: add `TripItem.tsx` component.

---
+++ src/components/TripItem.tsx
@@ -0,0 +1,39 @@
+import { Trip } from "@prisma/client";
+import Image from "next/image";
+import React from "react";
+import ReactCountryFlag from "react-country-flag";
+
+interface TripItemProps {
+  trip: Trip;
+}
+
+function TripItem({ trip }: TripItemProps) {
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[280px] w-[320px]">
+        <Image
+          style={{ objectFit: "cover" }}
+          className="rounded-lg shadow-md"
+          src={trip.coverImage}
+          alt={trip.name}
+          fill
+        />
+      </div>
+      <h3 className="text-primaryDarker font-medium text-sm mt-2">
+        {trip.name}
+      </h3>
+      <div className="flex item-center gap-1 my-1">
+        <ReactCountryFlag countryCode={trip.countryCode} svg />
+        <p className="text-xs text-grayPrimary">{trip.location}</p>
+      </div>
+      <p className="text-xs text-grayPrimary">
+        <span className="text-primaryDarker font-medium">
+          R${trip.pricePerDay.toString()}
+        </span>{" "}
+        por dia
+      </p>
+    </div>
+  );
+}
+
+export default TripItem;
