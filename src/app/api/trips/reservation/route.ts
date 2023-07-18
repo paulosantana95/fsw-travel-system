9ca4467744908789ff1ff4e1f70f77629534752f
Paulo Santana <paulosantana.sa@gmail.com>
2023-07-18 00:34:39 -0300

    feat: add trip buy

---
+++ src/app/api/trips/reservation/route.ts
@@ -0,0 +1,43 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const req = await request.json();
+
+  const { startDate, endDate, userId, tripId, totalPaid } = req
+
+  const trip = await prisma.trip.findUnique({
+    where: {
+      id: tripId,
+    },
+  });
+
+  if (!trip) {
+    return new NextResponse(
+      JSON.stringify({
+        error: {
+          code: "TRIP_NOT_FOUND",
+        },
+      }),
+      {
+        status: 404,
+      }
+    )
+  }
+
+  await prisma.tripReservation.create({
+    data: {
+      tripId,
+      startDate: new Date(startDate),
+      endDate: new Date(endDate),
+      userId,
+      totalPaid,
+    },
+  });
+
+  return new NextResponse(
+    JSON.stringify({
+      sucsess: true,
+    }), { status: 201, }
+  );
+}
--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -12,6 +12,8 @@ import { useRouter } from "next/navigation";
 import Button from "@/components/Button";
 
 import { Trip } from "@prisma/client";
+import { toast } from "react-toastify";
+import router from "next/router";
 
 function TripConfirmation({ params }: { params: { tripId: string } }) {
   const [trip, setTrip] = useState<Trip | null>();
@@ -19,7 +21,7 @@ function TripConfirmation({ params }: { params: { tripId: string } }) {
 
   const router = useRouter();
 
-  const { status } = useSession();
+  const { status, data } = useSession();
 
   const searchParams = useSearchParams();
 
@@ -31,7 +33,6 @@ function TripConfirmation({ params }: { params: { tripId: string } }) {
           tripId: params.tripId,
           startDate: searchParams.get("startDate"),
           endDate: searchParams.get("endDate"),
-          guests: searchParams.get("guests"),
         }),
       });
 
@@ -52,6 +53,35 @@ function TripConfirmation({ params }: { params: { tripId: string } }) {
   }, [status, searchParams, params, router]);
 
   if (!trip) return null;
+
+  const handleBuyClick = async () => {
+    console.log(data);
+    const res = await fetch("http://localhost:3000/api/trips/reservation", {
+      method: "POST",
+      body: Buffer.from(
+        JSON.stringify({
+          tripId: params.tripId,
+          startDate: searchParams.get("startDate"),
+          endDate: searchParams.get("endDate"),
+          guests: Number(searchParams.get("guests")),
+          userId: (data?.user as any).id!,
+          totalPaid: totalPrice,
+        })
+      ),
+    });
+
+    if (!res.ok) {
+      return toast.error("Ocorreu um erro ao realizar reserva!", {
+        position: "bottom-center",
+      });
+    }
+
+    router.push("/");
+
+    toast.success("Reserva realizada com sucesso!", {
+      position: "bottom-center",
+    });
+  };
 
   const startDate = new Date(searchParams.get("startDate") as string);
   const endDate = new Date(searchParams.get("endDate") as string);
@@ -103,7 +133,9 @@ function TripConfirmation({ params }: { params: { tripId: string } }) {
 
         <h3 className="font-semibold mt-5">Hóspedes</h3>
         <p>{guests} hóspedes</p>
-        <Button className="mt-5">Finalizar Compra</Button>
+        <Button className="mt-5" onClick={handleBuyClick}>
+          Finalizar Compra
+        </Button>
       </div>
     </div>
   );
