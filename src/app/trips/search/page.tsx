793301ae3781436557a84614d8e3ae4cdfdea0d2
Paulo Santana <paulosantana.sa@gmail.com>
2023-07-23 19:54:05 -0300

    style: adapt search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -29,16 +29,16 @@ function Trips() {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-col mp-5 items-center">
-      <h1 className="text-primaryDarker font-semibold text-xl">
+    <div className="container mx-auto flex flex-col p-5 items-center lg:items-start">
+      <h1 className="text-primaryDarker font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">
         Viagens Encontradas
       </h1>
-      <h2 className="text-grayPrimary font-medium mb-5">
+      <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">
         {trips.length > 0
           ? "Listamos as melhores viagens pra você!"
           : "Não econtramos nada no seus parâmetros! =("}{" "}
       </h2>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-5 lg:mt-6 lg:pb-16 lg:grid lg:gap-10 lg:grid-cols-4 lg:justify-center">
         {trips?.map((trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
