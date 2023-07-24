5e1f4a3d5f4a29eab26e50a43ab6f18c84ca6f3f
Paulo Santana <paulosantana.sa@gmail.com>
2023-07-24 01:13:38 -0300

    style: adapt trip search to desktop

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -81,7 +81,10 @@ function TripSearch() {
           />
         </div>
 
-        <Button onClick={() => handleSubmit(onSubmit)()} className="lg:w-1/2">
+        <Button
+          onClick={() => handleSubmit(onSubmit)()}
+          className="lg:w-1/2 lg:h-fit"
+        >
           Buscar
         </Button>
       </div>
